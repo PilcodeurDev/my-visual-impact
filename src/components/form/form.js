2e2de8bfbccb4ddd0ba6059278a1d0bc4a050b0b
simon despres <simondprs62790@hotmail.fr>
2024-01-10 16:05:49 +0100

    installation react-hook-form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,8 +1,14 @@
+'use client'
 import styles from './form.module.sass'
+import { useForm } from "react-hook-form"
 
 export default function Form() {
+
+const {register, handleSubmit, formState: { errors }} = useForm()
+const onSubmit = data => console.log(data)
+
   return (
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.caractere} >
           <div className={styles.form_group}>
             <input
@@ -10,7 +16,8 @@ export default function Form() {
               name="firstname"
               id="firstname"
               placeholder="Votre Prénom"
-            />
+              {...register("firstname")}
+              />
           </div>
           <div className={styles.form_group}>
             <input
@@ -18,7 +25,8 @@ export default function Form() {
               name="name"
               id="name"
               placeholder="Votre Nom"
-            />
+              {...register("name")}
+              />
           </div>
         </div>
         <div className={styles.form_group}>
@@ -26,14 +34,16 @@ export default function Form() {
             type="email"
             name="email"
             id="email"
-            placeholder="Votre e-mail"
-          />
+            placeholder="Votre E-mail"
+            {...register("e-mail")}
+            />
         </div>
         <div className={styles.form_group}>
           <textarea
             name="message"
             id="message"
-            placeholder="Votre message">
+            placeholder="Votre message"
+            {...register("message")}>
           </textarea>
         </div>
         <button type="submit">Envoyer le message</button>
